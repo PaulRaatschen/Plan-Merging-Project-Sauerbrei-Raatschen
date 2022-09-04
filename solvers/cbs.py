d9bf5ebc3b7413eda60e537261123b104aed32e7
PaulRaatschen <paul.raatschen@gmx.net>
2022-09-04 13:30:38 +0200

    removed parallel mode for macbs

--- solvers/cbs.py
+++ solvers/cbs.py
@@ -173,7 +173,7 @@ class CTNode:
         """
 
         meta_agent : Tuple[int] = self.conflic_matrix.meta_agents[agent-1]
-        ctl : Control = Control(['-Wnone','--parallel-mode=8,split'])
+        ctl : Control = Control(['-Wnone'])
         step : int = 0 
         cost : int = 0
         ret : SolveResult = None
